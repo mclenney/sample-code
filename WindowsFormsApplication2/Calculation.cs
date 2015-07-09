9242057317f7e36364d3f602b43e1163b4eeff4f
mclenney <mclenney@outlook.com>
2015-07-09 17:23:50 -0400

    Initialize Calculator

---
+++ WindowsFormsApplication2/Calculation.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Data;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApplication2
+{
+    class Calculation
+    {
+
+        public double Result = 0;
+        public String Message;
+        public bool IsError;
+        public String CleanEquation;
+
+        public Calculation(string input)
+        {
+            input = input.Trim().Replace(" ", "");
+            
+            Regex reg = new Regex("[-+*/]");
+            if(!reg.IsMatch(input))
+            {
+                Message = "Please insert an equation that contains one or more of the following operators: +, -, /, *.";
+                IsError = true;
+                return;
+            }
+
+            reg = new Regex("[^0-9-+*/]");
+            CleanEquation = reg.Replace(input, "");
+
+            try
+            {
+                Result = Convert.ToDouble(new DataTable().Compute(CleanEquation, null));
+
+            }
+            catch (Exception ex)
+            {
+                IsError = true;
+                //Log error if this was database connected
+                Type type = ex.GetType();
+                if(type.Name == "OverflowException")
+                {
+                    Message = "Error: the number that was equated was too big or small, please input a different equation.";
+                }
+            }
+
+            if (double.IsInfinity(Result))
+            {
+                IsError = true;
+                Message = "Error: unable to divide by zero, please input a different equation.";
+            }
+            else if(double.IsNaN(Result))
+            {
+                IsError = true;
+                Message = "Error: unable to divide zero by zero, please input a different equation.";
+            }
+        }
+    }
+}
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "";
+            button1.Text = "Calculate";
+            textBox1.Text = "";
+         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox1.Text))
+            {
+                Calculation c = new Calculation(textBox1.Text);
+                if(c.IsError)
+                {
+                    label1.Text = "Oops..there was an issue - " + c.Message;
+                }
+                else
+                {
+                    label1.Text = "The answser to '" + c.CleanEquation + "' is " + c.Result;
+                }
+            }
+            else
+            {
+                label1.Text = "Please input data before pressing the button";
+            }
+        }
+    }
+}
